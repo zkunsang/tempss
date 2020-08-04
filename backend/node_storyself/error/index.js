16a456112780de236dd4538513821dc267fbd02c
DESKTOP-N41VJ9H\illuni_08 <zkunsang@illuni.com>
2020-08-04 22:05:59 +0900

    test

--- backend/node_storyself/error/index.js
+++ backend/node_storyself/error/index.js
@@ -3,5 +3,4 @@ module.exports.Model = require('./ModelError');
 module.exports.Service = require('./ServiceError');
 module.exports.Dao = require('./DaoError');
 module.exports.Controller = require('./ControllerError');
-module.exports.RunTime = require('./RuntimeError');
 module.exports.Uncaught = require('./UncaughtError');
