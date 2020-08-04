368cc25a0aa5776891427bded3753848273b92fd
DESKTOP-N41VJ9H\illuni_08 <zkunsang@illuni.com>
2020-08-04 22:12:05 +0900

    test

--- backend/node_extensions/swagger/index.js
+++ backend/node_extensions/swagger/index.js
@@ -10,13 +10,14 @@ module.exports = {
         let apis = files.filter(m => (m.substr(-4) !== '_.js'));
         apis = apis.map(m => `${path}/${m}`);
 
+        const basePath = process.env.NODE_ENV === 'apiDev' ? `${apiConfig.apiUrl}`: `${apiConfig.apiUrl}:${apiConfig.port}`
         return swagger.init({
             apiVersion: '1.0',
             swaggerVersion: '1.0',
             swaggerURL: '/swagger',
             swaggerJSON: '/api-docs.json',
             swaggerUI: './public/swagger/',
-            basePath: `${apiConfig.apiUrl}:${apiConfig.port}`,
+            basePath: basePath,
             info: {
                 title: 'swagger-koa sample app',
                 description: 'Swagger + Koa = {swagger-koa}'
