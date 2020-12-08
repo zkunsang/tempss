20bfbdfe7682022194e72b3eca634037b621384f
Administrator <zkunsang@illuni.com>
2020-12-08 11:52:39 +0900

    config 삭제

--- frontend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    getBackendUrl: (() => {
-        return 'http://localhost:45000';
-    })(),
-    getCndUrl: (() => {
-        return 'http://cms-story.storyself.com/';
-    })()
-};
-
